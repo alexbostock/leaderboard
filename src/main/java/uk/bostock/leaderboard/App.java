4ef3418235425386bf6b7c5792acc82093d0020b
Alex Bostock <alex@bostock.uk>
2020-01-14 16:56:43 +0000

    Add additional response headers

--- src/main/java/uk/bostock/leaderboard/App.java
+++ src/main/java/uk/bostock/leaderboard/App.java
@@ -9,9 +9,21 @@ public class App {
 
         Handler handler = new Handler();
 
+        before((req, res) -> {
+            res.type("application/json");
+            res.header("Access-Control-Allow-Origin", "*");
+        });
+
         get("/scores", (req, res) -> handler.getAllScores(), gson::toJson);
         get("/scores/top/:n", (req, res) -> handler.getTopNScores(req.params("n")), gson::toJson);
         post("/scores", (req, res) -> handler.save(req), gson::toJson);
+
+        options("/scores", (req, res) -> {
+            res.header("Access-Control-Allow-Methods", "GET, POST");
+            res.header("Access-Control-Allow-Headers", "content-type");
+
+            return "";
+        });
 
         notFound("{\"status\": 404}");
         internalServerError("{\"status\": 500}");
